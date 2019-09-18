12fce073d3e07e9a2f31ddee3afca1d5349ceec0
Jeramy Wenserit <jeramy@digidip.net>
2019-09-18 11:17:02 +0200

    Rename MooMessage to AnimalMessage

--- src/adapters/example-adapter.ts
+++ src/adapters/example-adapter.ts
@@ -1,19 +1,19 @@
 import { TapjawAdapter, TapjawMessage } from 'tapjaw-importer';
 import ExampleHttpConnector, { ExampleResponse } from '../connectors/example-http-connector';
 
-export class MooMessage extends TapjawMessage {
+export class AnimalMessage extends TapjawMessage {
 }
 
 export default class ExampleAdapter implements TapjawAdapter {
     constructor(private readonly connector: ExampleHttpConnector) {
     }
 
-    public async * getAnimals(usePost = false): AsyncGenerator<MooMessage> {
+    public async * getAnimals(usePost = false): AsyncGenerator<AnimalMessage> {
         try {
             const results: ExampleResponse = usePost ? await this.connector.postAnimals() : await this.connector.getAnimals();
 
             for (const animal of results.data) {
-                yield new MooMessage('animals', animal);
+                yield new AnimalMessage('animals', animal);
             }
         } catch (err) {
             throw err;
